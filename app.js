ea5384fb1f87949381d59394a817188047a86c23
Sofia Mudry <sofiamudry@MBP-de-Sofi.fibertel.com.ar>
2022-04-16 11:34:03 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log('datosPersonales');
+
+alert('Hola, Bienvenide! Sign Up!');
+
+const nombre = prompt('Ingrese su nombre');
+const apellido = prompt('Ingrese su apellido');
+const edad = prompt('Ingrese su edad');
+const nacionalidad = prompt('Ingrese su nacionalidad');
+const documento = prompt('Ingrese su documento');
+
+alert(`Nuevo usuario agregado al sistema: 
+   ${nombre}
+   ${apellido}
+   ${edad}
+   ${nacionalidad}
+   ${documento}`);
